4ac16fce5d9679d4e5156cb5a034cc37f39cc774
sebastian lozano <sebaslozano708@gmail.com>
2024-12-15 19:09:15 -0500

    Improved sql query in the 'getAllBarbers' controller to improve performance

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -6,30 +6,58 @@ const getAllBarbers = async (req, res) => {
     const dbConnection = await database.getConnection();
     await dbConnection.beginTransaction();
 
-    let jsonData;
-
     try {
-        const [barbershops] = await dbConnection.execute("SELECT id, business_name, address, phone FROM barbershops");
 
-        const [images] = await dbConnection.execute("SELECT id, barbershop_id, image_path FROM barbershop_images");
+        const [barbershopsInfo] = await dbConnection.execute(`
+            SELECT 
+                barbershops.id AS barbershop_id,
+                business_name,
+                address,
+                phone,
+                barbershop_images.barbershop_id AS image_barberid,
+                image_path,
+                barbershop_services.id AS service_id,
+                barbershop_services.barbershop_id,
+                service
+            FROM barbershops
+            JOIN barbershop_images ON barbershop_images.barbershop_id = barbershops.id
+            JOIN barbershop_services ON barbershop_services.barbershop_id = barbershops.id;
+        `);
 
-        const [services] = await dbConnection.execute("SELECT barbershop_id, service, price FROM barbershop_services");
+        let structuredBarbershopsData = [];
+        
+        barbershopsInfo.forEach((barber) => {
+            if(!structuredBarbershopsData.some(curr => curr.barbershop_id === barber.barbershop_id)) {
 
-        jsonData = barbershops.map((barbershop) => {
-            const { id, business_name, address, phone } = barbershop;
-            return { 
-                barbershop_id: id, 
-                business_name, 
-                address, 
-                phone, 
-                images: images.filter((image) => image.barbershop_id === id), 
-                services: services.filter((service) => service.barbershop_id === id)
-            }
+                const { barbershop_id, business_name, address, phone } = barber;
+                structuredBarbershopsData.push({barbershop_id, business_name, address, phone, images: [], services: []})
+            };
+        });
+
+
+        barbershopsInfo.forEach((barber) => {
+            if(!structuredBarbershopsData.some(curr => curr.barbershop_id === barber.barbershop_id)) return;
+
+            structuredBarbershopsData. forEach((item) => {
+                if(item.barbershop_id === barber.barbershop_id && !item.images.includes(barber.image_path)){
+                    item.images.push(barber.image_path);
+                }
+            })
+        });
+
+        barbershopsInfo.forEach((barber) => {
+            if(!structuredBarbershopsData.some(curr => curr.barbershop_id === barber.barbershop_id)) return;
+
+            structuredBarbershopsData. forEach((item) => {
+                if(item.barbershop_id === barber.barbershop_id && !item.services.includes(barber.service)){
+                    item.services.push(barber.service);
+                }
+            })
         });
 
         await dbConnection.commit();
 
-        res.status(200).json(jsonData);
+        res.status(200).json(structuredBarbershopsData);
     }
     catch(error){
         await dbConnection.rollback();
